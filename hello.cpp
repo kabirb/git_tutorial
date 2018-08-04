610d7cf5f7d8aec9cb1584e170f28c406921c4a7
kabirb <kabirb@sfu.ca>
2018-08-04 15:10:54 -0700

    Added to hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 	
 int main()
 {
+    cout << " Dont talk like that" << endl;
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
